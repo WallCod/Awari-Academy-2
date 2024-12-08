44bd3ac4489b37a87554fb44c980f9d615aadb30
Wallax Figueiredo <amadocarilloleiva@gmail.com>
2024-12-08 19:16:37 -0300

    Node Core Modules

--- 7 - Node Core Modules/Exemplo 2/url.js
+++
@@ -1,9 +0,0 @@
-const url = require('url');
-
-const baseUrl = 'https://www.example.com/path/';
-
-const relativeUrl = '../otherpath/page.html';
-
-const resolvedUrl = url.resolve(baseUrl, relativeUrl);
-
-console.log(`URL resolvida: ${resolvedUrl}`);
